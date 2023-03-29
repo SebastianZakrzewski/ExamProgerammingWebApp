de77a9d512d774c78e55e23888d3a65aefc3e826
sebastian <sebastianzakrzewski1995@gmai.com>
2023-03-29 22:23:06 +0200

    Add back-end files

---
+++ BackEnd/src/test/java/com/example/smileshop/SmileShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smileshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
